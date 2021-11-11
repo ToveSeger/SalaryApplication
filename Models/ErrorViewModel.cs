6dc7fed99a8b4a73b3414068d9bdf93188bd64c7
toveseger <tove.seger@outlook.com>
2021-11-11 16:03:46 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace SalaryApplication.Models
+{
+    using System;
+
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SalaryApplicationTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using SalaryApplication.Controllers;
+
+namespace SalaryApplication.Controllers.Tests
+{
+    [TestFixture()]
+    public class HomeControllerTests
+    {
+        [Test()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
+namespace SalaryApplicationTests.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class HomeControllerTests
+    {
+    }
+}
