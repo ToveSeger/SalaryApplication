e05242c87afd8ecb1c9b644e06b29f2c39944cfe
Jesper Persson <anders.jesper.persson@gmail.com>
2021-11-12 16:29:38 +0100

    added login-funciton

---
+++ SalaryApplication/Controllers/LoginAndLogoutController.cs
@@ -0,0 +1,55 @@
+﻿namespace SalaryApplication.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using SalaryApplication.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class LoginAndLogoutController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+        public LoginAndLogoutController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: LoginController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LoginVerification(string password, string username)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.UserName == username && u.PassWord == password);
+            if (user != null)
+            {
+                TempData["user"] = user.Id;
+                TempData.Keep();
+
+                return View("LoginSucced", user);
+            }
+
+            var user1 = _context.Admins.FirstOrDefault(u => u.UserName == username && u.PassWord == password);
+            if (user1 != null)
+            {
+                TempData["user"] = user1.Id;
+                TempData.Keep();
+
+                return View("LoginSucced", user1);
+            }
+
+            return View("LoginFailed");
+        }
+
+        public ActionResult Logut()
+        {
+            TempData.Clear();
+            return View("Logout");
+        }
+
+    }
+}
+
