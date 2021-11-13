5b166478654f66ddd65c40ac525796f358ccc98b
Jesper Persson <anders.jesper.persson@gmail.com>
2021-11-13 11:40:57 +0100

    fixed userView and namespaces

--- SalaryApplication/Models/Admin.cs
+++ SalaryApplication/Models/Admin.cs
@@ -2,11 +2,13 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Threading.Tasks;
 
     public class Admin: User
     {
+        [Display(Name = "Admin")]
         public bool IsAdmin { get; set; }
     }
 }
