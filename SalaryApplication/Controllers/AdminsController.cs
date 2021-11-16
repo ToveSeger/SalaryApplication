62cb45a62bad93c6f6c4e8a8ca6720888569ba84
toveseger <tove.seger@outlook.com>
2021-11-16 12:09:24 +0100

    More tests for admin controller done as well as commenting in controller and test class.

--- SalaryApplication/Controllers/AdminsController.cs
+++ SalaryApplication/Controllers/AdminsController.cs
@@ -10,6 +10,9 @@ using SalaryApplication.Models;
 
 namespace SalaryApplication.Controllers
 {
+    /// <summary>
+    /// Controls admin functionality
+    /// </summary>
     public class AdminsController : Controller
     {
         private readonly ApplicationDBContext _context;
@@ -19,8 +22,12 @@ namespace SalaryApplication.Controllers
             _context = context;
         }
 
-
-        // GET: Admins
+        /// <summary>
+        /// Gets the admin profile view if user is logged in as admin. 
+        /// If user is logged in as non admin, NotAdmin is presented.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>The correct view depending on above mentioned parameters </returns>
         public async Task<IActionResult> Index(int? id)
         {
             if (TempData.ContainsKey("user"))
@@ -31,13 +38,17 @@ namespace SalaryApplication.Controllers
             var user = _context.Admins.FirstOrDefault(u => u.Id==id);
             if (user != null && user.IsAdmin)
             {
-                return View(await _context.Admins.ToListAsync());
+                return View("Index", await _context.Admins.ToListAsync());
             }
             else return View("NotAdmin");
         }
 
 
-        // GET: Admins/Details/5
+        /// <summary>
+        /// Gets the details of the specified id. 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>View with details of specified id if user exists, otherwhise NotFound view</returns>
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -55,15 +66,20 @@ namespace SalaryApplication.Controllers
             return View(admin);
         }
 
-        // GET: Admins/Create
+        /// <summary>
+        /// Gets creation view
+        /// </summary>
+        /// <returns>Creation view</returns>
         public IActionResult Create()
         {
             return View("Create");
         }
 
-        // POST: Admins/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Creates a new user/admin with the specified bind parameters if modelstate is valid.  
+        /// </summary>
+        /// <param name="admin"></param>
+        /// <returns>Index view of admin</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("IsAdmin,Salary,Role,Id,EmployeeNumber,FirstName,LastName,UserName,PassWord")] Admin admin)
@@ -78,7 +94,11 @@ namespace SalaryApplication.Controllers
             return View(admin);
         }
 
-        // GET: Admins/Edit/5
+        /// <summary>
+        /// Presents edit view.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Edit view as long as user is admin, otherwhise NotFound view</returns>
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -94,9 +114,12 @@ namespace SalaryApplication.Controllers
             return View(admin);
         }
 
-        // POST: Admins/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Makes it possible to edit a specified user as long as id is found. 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="admin"></param>
+        /// <returns>Admin index view if action is carried out, otherwhise NotFound view</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("IsAdmin,Salary,Role,Id,EmployeeNumber,FirstName,LastName,UserName,PassWord")] Admin admin)
@@ -129,7 +152,11 @@ namespace SalaryApplication.Controllers
             return View(admin);
         }
 
-        // GET: Admins/Delete/5
+        /// <summary>
+        /// Presents deletion view as long as user is admin.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Deletion view as long as user is admin, otherwhise NotFound view</returns>
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -147,7 +174,12 @@ namespace SalaryApplication.Controllers
             return View(admin);
         }
 
-        // POST: Admins/Delete/5
+
+        /// <summary>
+        /// Makes it possible to delete it a specified admin user as long as id is found. 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Admin index view if action is carried out, otherwhise NotFound view</returns>
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
@@ -158,11 +190,23 @@ namespace SalaryApplication.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        /// <summary>
+        /// Checks whether an admin exists or not.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>false if admin doesn't exist, true if it exists</returns>
         public bool AdminExists(int id)
         {
             return  _context.Admins.Any(e => e.Id == id);
         }
 
+        /// <summary>
+        /// Controls that the username & password is correct for the specified user.If so: delete user.
+        /// </summary>
+        /// <param name = "username" ></ param >
+        /// < param name= "password" ></ param >
+        /// < param name= "id" ></ param >
+        /// < returns > Index view of admin</returns>
         public async Task<IActionResult> DeleteUser(string username, string password, int? id)
         {
             var user = _context.Admins.FirstOrDefault(e => e.Id == id);
@@ -174,6 +218,6 @@ namespace SalaryApplication.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return RedirectToAction(nameof(Index));
+            }
         }
-    }
 }
--- SalaryApplicationTests1/Controllers/AdminsControllerTests.cs
+++ SalaryApplicationTests1/Controllers/AdminsControllerTests.cs
@@ -6,14 +6,59 @@ using SalaryApplication.Models;
 using System.Data.Entity;
 using Microsoft.Extensions.DependencyInjection;
 using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+using Microsoft.AspNetCore.Http;
 
 namespace SalaryApplication.Controllers.Tests
 {
+    /// <summary>
+    /// Tests methods in AdminsController class
+    /// </summary>
     [TestFixture()]
     public class AdminsControllerTests
     {
         private ApplicationDBContext _context;
 
+        /// <summary>
+        /// Makes sure admins are routed admin-view when clicking admin profile
+        /// </summary>
+        [Test()]
+        public void IndexTestForAdmin()
+        {
+            _context = new ApplicationDBContext();
+            AdminsController controller = new AdminsController(_context);
+            ITempDataProvider tempDataProvider = Mock.Of<ITempDataProvider>();
+            TempDataDictionaryFactory tempDataDictionaryFactory = new TempDataDictionaryFactory(tempDataProvider);
+            ITempDataDictionary tempData = tempDataDictionaryFactory.GetTempData(new DefaultHttpContext());
+            controller.TempData = tempData;
+            var user = _context.Admins.FirstOrDefault(u => u.IsAdmin == true);
+            var result = controller.Index(user.Id).Result as ViewResult;
+            Assert.AreEqual("Index", result.ViewName);
+        }
+
+        /// <summary>
+        /// Makes sure non admins are routed no non admin-view when trying to access admin view
+        /// </summary>
+        [Test()]
+        public void IndexTestForNonAdmin()
+        {
+            _context = new ApplicationDBContext();
+            AdminsController controller = new AdminsController(_context);
+            ITempDataProvider tempDataProvider = Mock.Of<ITempDataProvider>();
+            TempDataDictionaryFactory tempDataDictionaryFactory = new TempDataDictionaryFactory(tempDataProvider);
+            ITempDataDictionary tempData = tempDataDictionaryFactory.GetTempData(new DefaultHttpContext());
+            controller.TempData = tempData;
+            var user = _context.Admins.FirstOrDefault(u => u.IsAdmin == false);
+            var result = controller.Index(user.Id).Result as ViewResult;
+            Assert.AreEqual("NotAdmin", result.ViewName);
+        }
+
+        /// <summary>
+        /// Makes sure create-view is presented when pressing create button
+        /// </summary>
         [Test()]
         public void CreateTest()
         {
@@ -21,7 +66,11 @@ namespace SalaryApplication.Controllers.Tests
             var result = controller.Create() as ViewResult;
             Assert.AreEqual("Create", result.ViewName);
         }
-      
+
+        /// <summary>
+        /// Creates admin object to be used in internal test methods. 
+        /// </summary>
+        /// <returns>Admin object</returns>
         internal Admin createAdminObject()
         {
             Admin admin = new Admin();
@@ -37,8 +86,11 @@ namespace SalaryApplication.Controllers.Tests
             return admin;
         }
 
-
-        [Test()] //Integration test
+        /// <summary>
+        /// **Integration test**
+        /// Makes sure adding admin works, as well as finding the admin when it's created. 
+        /// </summary>
+        [Test()] 
         public void CreateTestShouldAddAdmin()
         {
             _context = new ApplicationDBContext();
@@ -50,13 +102,19 @@ namespace SalaryApplication.Controllers.Tests
             Assert.IsTrue(addedUser);
         }
 
-        [Test()] //Integration test
+        /// <summary>
+        /// **Integration test**
+        /// Makes sure deletion of admin works, as well as searching for admin (and not finding it) when deletion is carried out.
+        /// </summary>
+        [Test()]
         public void DeleteTestShouldDeleteAdmin()
         {
             _context = new ApplicationDBContext();
             AdminsController controller = new AdminsController(_context);
             var admin = createAdminObject();
-            System.Threading.Tasks.Task<IActionResult> deleteAdmin = controller.DeleteConfirmed(admin.Id);
+            admin = _context.Admins.FirstOrDefault(e => e.UserName == admin.UserName && e.PassWord == admin.PassWord);
+            System.Console.WriteLine(admin.Id);
+            System.Threading.Tasks.Task<IActionResult> deleteAdmin = controller.DeleteUser(admin.UserName, admin.PassWord, admin.Id);
             Thread.Sleep(2000);
             var addedUser = controller.AdminExists(admin.Id);
             Assert.IsFalse(addedUser);
